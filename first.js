518fb5ff517637d1537b08fd8dd38b236b39029c
aks200 <akskn200@gmail.com>
2025-04-24 16:18:45 +1200

    addind starting project

---
+++ first.js
@@ -0,0 +1,9 @@
+fetch('https://api.recruitment.shq.nz/domains/{client_id}')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error('Fetch error:', error));
