9679923e642a6b7719abedfd5fb1d4b86c7aa6ff
aks200 <akskn200@gmail.com>
2025-04-25 00:52:41 +1200

    first commit

--- first.js
+++ first.js
@@ -1,9 +1,34 @@
-fetch('https://api.recruitment.shq.nz/domains/{client_id}')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+
+
+const apiKey = 'h523hDtETbkJ3nSJL323hjYLXbCyDaRZ';
+const apiUrl = 'https://api.recruitment.shq.nz';
+
+async function getItems() {
+  try {
+    const response_id = await fetch('https://api.recruitment.shq.nz/domains/{client_id}'); // replace with your API endpoint
+    if (!response_id.ok) {
+      throw new Error(`HTTP error! Status: ${response_id.status}`);
     }
-    return response.json();
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error('Fetch error:', error));
+
+    const data = await response_id.json();
+    console.log('client_id:', data); // data should be an array of items
+    return data;
+  } catch (error) {
+    console.error('Error fetching items:', error);
+    return [];
+  }
+  try {
+    const response_z_id = await fetch('https://api.recruitment.shq.nz/zones/{zone_id}'); // replace with your API endpoint
+    if (!response_z_id.ok) {
+      throw new Error(`HTTP error! Status: ${response_z_id.status}`);
+    }
+
+    const data1 = await response_z_id.json();
+    console.log('zone_id:', data1); // data should be an array of items
+    return data;
+  } catch (error) {
+    console.error('Error fetching items:', error);
+    return [];
+  }
+}
+
